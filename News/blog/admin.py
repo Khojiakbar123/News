d2a2007e5230a49a59dd17a2891472660ed2f7c2
Khojiakbar123 <62052094+Khojiakbar123@users.noreply.github.com>
2024-10-07 15:01:38 +0500

    Add files via upload

---
+++ News/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Category, Article, CustomUser
+
+# Register your models here.
+
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(CustomUser)
